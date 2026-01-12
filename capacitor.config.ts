46c2647fd62c8e967223222e7fb71577cd77cdf3
brandonogram <mcrbrandon@gmail.com>
2026-01-12 08:27:38 -0500

    Initial commit with Next.js, Capacitor, and PWA setup

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.photoboother.christmaslighttakedownoptimizer',
+  appName: 'christmas-light-takedown-optimizer',
+  webDir: 'out'
+};
+
+export default config;
